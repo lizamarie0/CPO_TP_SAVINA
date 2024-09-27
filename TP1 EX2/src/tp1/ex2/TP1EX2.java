d820844e1dffc561609839341e24138078a611ce
Liza Savina <Liza Savina@10.3.61.218>
2024-09-27 10:31:59 +0200

    TP1 E21 manipnombreint

--- TP1 EX2/src/tp1/ex2/TP1EX2.java
+++ TP1 EX2/src/tp1/ex2/TP1EX2.java
@@ -19,47 +19,52 @@ public class TP1EX2 {
     public static void main(String[] args) {
         // TODO code application logic here
         double Celcius;
-        double Kelvin;
+        double Kelvin; 
+        double b;
 Scanner sc = new Scanner(System.in);
-System.out.println("\n Entrer le nombre :");
-Celcius=sc.nextInt(); // On demande a sc de donner le prochain entier
-Kelvin = Celcius + 273;
-System.out.println("\n la temperature en kelvin est" + Kelvin);
-    }
-    
-    
-    double Celcius;
-    double Kelvin;
-   
-    public static double CelciusVersKelvin (double Celcius) {
-        Celcius = Celcius + 273 ;
-        return (Kelvin);
-}
-    
-    public static double KelvinVersCelcius (double Kelvin) {
-        Kelvin = Kelvin - 273;
-        return (Kelvin);
-}
-    
-    public static double FarenheitVersCelcius (double Farenheit) {
-        Farenheit = (Farenheit - 32) * 5/9 ;
-        return (Farenheit);
+System.out.println("\n Entrer Celcius :");
+System.out.println("\n commande :  " + "1) CelciusVersKelvin" + "2) KelvinVersCelcius" + "3) FarenheitVersCelcius" + "4) CelciusVersFarenheit" + "5) KelvinVersFarenheit" + "6) FarenheitVersKelvin");
+b=sc.nextInt(); // On demande a sc de donner le prochain entier
 
 
+    
+    if (b==1) {
+        public static double CelciusVersKelvin (double Celcius) {
+            Celcius = Celcius + 273 ;
+            return (Kelvin);
+
+    
+    if (b==2) {
+        public static double KelvinVersCelcius (double Kelvin) {
+            Kelvin = Kelvin - 273;
+            return (Kelvin);
 }
-    public static double CelciusVersFarenheit (double Celcius) {
-        Celcius = (Celcius * 1.8) + 32 ;
-        return (Celcius);
-        
+    if (b==3) {
+        public static double FarenheitVersCelcius (double Farenheit) {
+            Farenheit = (Farenheit - 32) * 5/9 ;
+            return (Farenheit);
+            
+}
+
+}   
+    if (b==4) {
+        public static double CelciusVersFarenheit (double Celcius) {
+            Celcius = (Celcius * 1.8) + 32 ;
+            return (Celcius);
+     
+
     }
     
-    public static double KelvinVersFarenheit (double Kelvin) {
-        Kelvin = Kelvin * 9.5 + 459.67 ;
-        return (Kelvin);
+    if (b==5) {
+        public static double KelvinVersFarenheit (double Kelvin) {
+            Kelvin = Kelvin * 9.5 + 459.67 ;
+            return (Kelvin);
     }
     
-    public static double FarenheitVersKelvin (double Farenheit) {
-        Farenheit = (Farenheit - 32)/1.8 +273.15;
-        return (Farenheit);
+
+    if (b==5) {
+        public static double FarenheitVersKelvin (double Farenheit) {
+            Farenheit = (Farenheit - 32)/1.8 +273.15;
+            return (Farenheit);
     }
 }
