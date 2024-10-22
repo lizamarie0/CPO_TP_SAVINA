10bd8a957a7e5e84dd422e3d1fd97417c9b80623
Liza Savina <Liza Savina@10.3.61.218>
2024-10-22 21:02:23 +0200

    TP2 EX2

---
+++ nbcalories/src/nbcalories/Nbcalories.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nbcalories;
+
+/**
+ *
+ * @author Liza Savina
+ */
+public class Nbcalories {
+
+    public static void main(String[] args) {
+        class NbCalories {
+
+        public void Tartiflette ( ) {
+            int NbCalories;
+        }
+        
+    public void Moussaka() {
+        System.out.println("La valeur de Pi est 3.1415927");
+            int NbCalories;
+    }
+}
+}
+}
+
