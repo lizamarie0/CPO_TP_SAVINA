46a2af73f0388aca189f0a1f64464947a153f615
Liza Savina <Liza Savina@10.3.61.218>
2024-10-23 11:16:55 +0200

    TP2 EX4

---
+++ TP2 EX4/src/tp2/ex4/TP2EX4.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2.ex4;
+
+/**
+ *
+ * @author Liza Savina
+ */
+public class TP2EX4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        voiture premiere = new voiture ();
+        
+        String model;
+        String Marque;
+        int puissanceCV;
+        
+        Personne seconde = new Personne ();
+        String nom;
+        String prenom;
+        
+        
+        
+        
+        
+        
+    }
+             
+}
