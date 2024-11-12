60e5bc18c77bcac90c73fe7321e8acb7ce2f0511
Liza Savina <Liza Savina@10.3.61.218>
2024-11-12 23:25:41 +0100

    TP2_Tartiflette_version2

---
+++ TP2_manip_version2/src/tp2_manip_version2/Tartiflette_version2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_version2;
+
+/**
+ *
+ * @author Liza Savina
+ */
+public class Tartiflette_version2 {
+        int nbCalories;
+    
+    public Tartiflette_version2(int a){
+        this.nbCalories = a;
+    }
+
+}
+
