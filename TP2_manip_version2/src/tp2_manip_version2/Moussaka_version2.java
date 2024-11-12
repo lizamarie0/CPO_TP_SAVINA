92ce68749cd5d2f027cd40f0e3c186b649fd1ce0
Liza Savina <Liza Savina@10.3.61.218>
2024-11-12 23:26:15 +0100

    TP2_Moussaka_version2

---
+++ TP2_manip_version2/src/tp2_manip_version2/Moussaka_version2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_version2;
+
+/**
+ *
+ * @author Liza Savina
+ */
+public class Moussaka_version2 {
+    int nbCalories;
+    
+    public Moussaka_version2(int a){
+        this.nbCalories = a;
+    }
+}
